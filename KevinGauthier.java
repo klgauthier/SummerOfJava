f379e5e91e860daeb819b0dade35aea590f0dd4b
Raymundo Yu <raymaryu@hotmail.com>
2014-07-10 14:01:31 -0300

    Initializing

---
+++ KevinGauthier.java
@@ -0,0 +1,20 @@
+
+/**
+ * Kevin Gauthier class!
+ * 
+ * @author Kevin Gauthier
+ * @version 07.07.14
+ */
+public class KevinGauthier extends Person
+{
+    public KevinGauthier()
+    {
+        super("Kevin Gauthier", 21, Gender.MALE, "Brown", "Blue"); // pass parameters to base class
+    }
+    
+    public String getSaying()
+    {
+        return "Fact. Bears eat beets. Bears. Beets. Battlestar Galactica."; 
+        // return personalized saying
+    }
+}
