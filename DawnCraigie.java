01ddeef1f16113045e94c341190dd488182c3a96
Dawn Craigie <Dawn.Craigie@hotmail.com>
2014-07-10 13:38:26 -0300

    Added in DawnCraigie

---
+++ DawnCraigie.java
@@ -0,0 +1,19 @@
+
+/**
+ * Dawn Craigie
+ * 
+ * @author Dawn Craigie
+ * @version 07.08.14
+ */
+public class DawnCraigie extends Person
+{
+    public DawnCraigie()
+    {
+        super("Dawn Craigie", 59, Gender.FEMALE, "Mocha", "Blue");
+    }
+    
+    public String getSaying()
+    {
+        return "Women are like wine - they get better with age.";
+    }
+}
