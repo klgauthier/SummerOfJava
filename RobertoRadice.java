f8cabe42077dd8ccb0f0b9b549d3361412d1d0e1
Amanda Champion <husky28@hotmail.com>
2014-07-10 14:58:04 -0300

    Updated

--- RobertoRadice.java
+++ RobertoRadice.java
@@ -13,6 +13,6 @@ public class RobertoRadice extends Person
     
     public String getSaying()
     {
-        return "I am a rock ... something";//return saying
+        return "I am a rock ... something or other";//return saying
     }
 }
